7ac2e73478f39e8b98038ef4348bcb809d2166d4
Martin Sumner <martin.sumner@adaptip.co.uk>
2018-07-17 10:34:52 +0100

    Revert "Allow force on rebuid_trees"
    This reverts commit e495d34fcd5911fe8099115a0c41a338f5eaa5b8.

--- test/end_to_end/mock_kv_vnode.erl
+++ test/end_to_end/mock_kv_vnode.erl
@@ -409,8 +409,7 @@ handle_cast({rebuild_complete, store}, State) ->
     ok = aae_controller:aae_rebuildtrees(State#state.aae_controller, 
                                             State#state.index_ns,
                                             State#state.preflist_fun,
-                                            Worker,
-                                            true),
+                                            Worker),
 
     {noreply, State#state{aae_rebuild = true}};
 handle_cast({rebuild_complete, tree}, State) ->
